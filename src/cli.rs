c29d145df80ebeab7ea032a544a46f2422a773b0
Jormungand <diojormungand@gmail.com>
2023-02-02 09:57:50 +0300

    новый файл:    src/cli.rs 	изменено:      Cargo.toml 	изменено:      src/main.rs

---
+++ src/cli.rs
@@ -0,0 +1,19 @@
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+pub enum Action {
+    ///Start task
+    #[structopt()]
+    Add { task: String},
+    #[structopt()]
+    Done { position: usize},
+    List,
+}
+#[derive(Debug, StructOpt)]
+pub struct CommandLineArgs {
+    #[structopt(subcommand)]
+    pub action: Action,
+    #[structopt(parse(from_os_str), short, long)]
+    pub journal_file: Option<PathBuf>,
+}
